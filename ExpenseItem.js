8ccd6e594b7ef8c3d015b8f3c989c0b14d3f82e1
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-07 01:50:15 +0530

    till composition with details seperation

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
 
 
 
-function ExpenseItem(props){
+const  ExpenseItem = (props) => {
     
 
     return(
     <Card className='expense-item'>
        <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
-            
             <h2>{props.title}</h2>
-            <div className='expense-item__LocationOfExpendeture'>{props.location}</div>
-            <div className='expense-item__price'>${props.amount}</div>
+            
+            
+            
+            
+            
+            <p className="expense-item__description">{props.location}</p>
+            <ExpenseDetails amount={props.amount} />
+
+
+
+
+            
            
             
         </div>
