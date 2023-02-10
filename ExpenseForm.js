ecd53e3282755c2d23a088b6a4f6b6d39af129f1
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-10 15:16:13 +0530

    commit form submitting

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -39,9 +39,17 @@ const ExpenseForm = () => {
     //})
     setEnteredDate(event.target.value);
    };
-
+   const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData={
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+    console.log(expenseData);
+   };
     return (
-    <form action="">
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             
             <div className="new-expense__control">
@@ -58,7 +66,7 @@ const ExpenseForm = () => {
                 <input type="date"   min="2019-01-01"   max="2022-12-31" onChange={dateChangeHandler}/>
             </div>
             <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+                <button type="submit" >Add Expense</button>
             </div>
     </form>
     );
