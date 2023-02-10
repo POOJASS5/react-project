85cc1de7c85d49c28d04eb8a044ea9e067013370
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-10 20:56:40 +0530

    filter assignment 3

--- Expenses.js
+++ Expenses.js
@@ -8,14 +8,17 @@ import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
     return (
       <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => {
+        {filteredExpenses.map((expense) => {
           return(
             <ExpenseItem
             key={expense.id}
